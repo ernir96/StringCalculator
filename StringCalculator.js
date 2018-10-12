74df5ce550d35c1861a6445addef77a73ce25559
Ernir Snær Helgason <ernir@Ernirs-MBP.hringdu.is>
2018-10-12 17:13:56 +0000

    added functionality and test to not allow negative numbers

--- StringCalculator.js
+++ StringCalculator.js
@@ -6,7 +6,34 @@ var StringCalculator = {
 	    return 0;
 	  }
     else {
+      var negativeNumbers = [];
       var arrNumber = numbers.split(/,|\n/g);
+
+      var negativeExists = false;
+      var j = 0;
+
+      for(i = 0; i < arrNumber.length; i++) {
+        if (arrNumber[i] < 0) {
+          negativeNumbers[j] = arrNumber[i];
+          j++;
+          negativeExists = true;
+        }
+      }
+
+
+      negativeList = "";
+      if (negativeExists) {
+        for(i = 0; i < negativeNumbers.length - 1; i++) {
+          negativeList += negativeNumbers[i] + ",";
+        }
+        negativeList += negativeNumbers[negativeNumbers.length - 1];
+      }
+
+
+      if (negativeExists) {
+        return "Negatives not allowed: " + negativeList;
+      }
+
       var sum = 0;
       for(i = 0; i < arrNumber.length; i++) {
         sum += parseInt(arrNumber[i]);
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -21,3 +21,11 @@ test("Testing unknown number of numbers", () => {
 test("Testing to allow newline or 'comma' to split numbers", () => {
   expect(StringCalculator.Add("8,7\n12,11\n1")).toBe(39);
 });
+
+test("Testing to not allow negative numbers", () => {
+  expect(StringCalculator.Add("2,-4,3,-5")).toBe("Negatives not allowed: -4,-5");
+});
+
+test("Testing to not allow negative numbers when 1 negative", () => {
+  expect(StringCalculator.Add("2,3,-5")).toBe("Negatives not allowed: -5");
+});
